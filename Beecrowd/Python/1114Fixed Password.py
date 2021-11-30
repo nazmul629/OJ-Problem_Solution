3f8459e51def204d75bd9a9c68c4ed840842aff2
nazmul629 <nazmulhossain.qnh@gmail.com>
2021-11-30 12:17:59 +0600

    1957

---
+++ Beecrowd/Python/1114Fixed Password.py
@@ -0,0 +1,9 @@
+
+is_valid = True
+while is_valid:
+    password = int(input())
+    if password == 2002:
+        print("Acesso Permitido")
+        is_valid =False
+    else:
+        print("Senha Invalida")
---
+++ Beecrowd/Python/1957 Converting to Hexadecimal.py
@@ -0,0 +1,4 @@
+n = int(input())
+if n>=1:
+    hx = ("%#X"%n)
+print(hx[2:])
