2a3bd52d5405fbfc724d51dd5a9665b3d415a14d
nazmul629 <nazmulhossain.qnh@gmail.com>
2021-11-30 15:59:52 +0600

    1012

---
+++ Beecrowd/Python/1012 - Area.py
@@ -0,0 +1,16 @@
+A,B,C = map(float,input().split())
+
+result = .5*A*C 
+pi = 3.14159
+result = .5*A*C 
+print(f"TRIANGULO: {result:.3f}")
+
+result = pi *C*C
+print(f"CIRCULO: {result:.3f}")
+result = (A+B)/2 *C
+print(f"TRAPEZIO: {result:.3f}")
+
+result = B*B
+print(f"QUADRADO: {result:.3f}")
+result = A*B
+print(f"RETANGULO: {result:.3f}")
