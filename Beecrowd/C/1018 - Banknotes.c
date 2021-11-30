5d244fbbd2c8ab5c3b934302bb7afe2a27c16fe6
nazmul629 <nazmulhossain.qnh@gmail.com>
2021-11-30 16:52:47 +0600

    1018

---
+++ Beecrowd/C/1018 - Banknotes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{   
+    int notes,x ;
+
+    scanf("%d", &notes);
+
+    printf("%d\n", notes);
+
+    printf("%d nota(s) de R$ 100,00\n", notes/100);
+    x = (notes%100);
+
+    printf("%d nota(s) de R$ 50,00\n", x/50);
+    x = (x%50);
+
+    printf("%d nota(s) de R$ 20,00\n", x/20);                                                                                                                        
+    x = (x%20);
+
+    printf("%d nota(s) de R$ 10,00\n", x/10);
+    x = (x%10);
+
+    printf("%d nota(s) de R$ 5,00\n", x/5);
+    x = (x%5);
+
+    printf("%d nota(s) de R$ 2,00\n", x/2);
+    x = (x%2);
+
+    printf("%d nota(s) de R$ 1,00\n", x/1);
+    
+    return 0;
+
+}
